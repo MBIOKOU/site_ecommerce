cad9a0438edcb6c57a087d89f9a3685295c4bc38
MBIOKOU <mourbiokou8@gmail.com>
2025-02-10 12:01:58 +0100

    Version

--- front/games.js
+++
@@ -1,9 +0,0 @@
-// Gérer les clics sur la navigation
-document.querySelectorAll('.li').forEach(item => {
-    item.addEventListener('click', () => {
-        document.querySelectorAll('.li').forEach(nav => nav.classList.remove('active'));
-        item.classList.add('active');
-        const section = item.getAttribute('data-section');
-        loadSection(section);
-    });
-});
